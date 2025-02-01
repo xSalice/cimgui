2fa99667703a5973aba445a986398697247228ab
xSalice <xsalicez@gmail.com>
2025-02-01 14:29:35 -0600

    implementations

--- generator/output/cimgui_impl.h
+++ generator/output/cimgui_impl.h
@@ -87,3 +87,35 @@ CIMGUI_API bool ImGui_ImplSDL3_ProcessEvent(const SDL_Event* event);
 CIMGUI_API void ImGui_ImplSDL3_SetGamepadMode(ImGui_ImplSDL3_GamepadMode mode,SDL_Gamepad** manual_gamepads_array,int manual_gamepads_count);
 
 #endif
+#ifdef CIMGUI_USE_DX11
+
+typedef struct ID3D11Device ID3D11Device;
+typedef struct ID3D11DeviceContext ID3D11DeviceContext;
+typedef struct ID3D11SamplerState ID3D11SamplerState;
+typedef struct ID3D11Buffer ID3D11Buffer;
+struct ID3D11Device;
+struct ID3D11DeviceContext;
+struct ID3D11SamplerState;
+struct ID3D11Buffer;
+typedef struct ImGui_ImplDX11_RenderState ImGui_ImplDX11_RenderState;
+struct ImGui_ImplDX11_RenderState
+{
+    ID3D11Device* Device;
+    ID3D11DeviceContext* DeviceContext;
+    ID3D11SamplerState* SamplerDefault;
+    ID3D11Buffer* VertexConstantBuffer;
+};CIMGUI_API bool ImGui_ImplDX11_Init(ID3D11Device* device,ID3D11DeviceContext* device_context);
+CIMGUI_API void ImGui_ImplDX11_Shutdown(void);
+CIMGUI_API void ImGui_ImplDX11_NewFrame(void);
+CIMGUI_API void ImGui_ImplDX11_RenderDrawData(ImDrawData* draw_data);
+CIMGUI_API bool ImGui_ImplDX11_CreateDeviceObjects(void);
+CIMGUI_API void ImGui_ImplDX11_InvalidateDeviceObjects(void);
+
+#endif
+#ifdef CIMGUI_USE_WIN32
+CIMGUI_API bool ImGui_ImplWin32_Init(void* hwnd);
+CIMGUI_API bool ImGui_ImplWin32_InitForOpenGL(void* hwnd);
+CIMGUI_API void ImGui_ImplWin32_Shutdown(void);
+CIMGUI_API void ImGui_ImplWin32_NewFrame(void);
+
+#endif
